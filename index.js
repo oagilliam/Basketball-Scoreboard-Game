9a5d066b98d377a58dfde8a9b1be3f533d199ff0
Olivia Gilliam <oagilliam@outlook.com>
2023-08-20 13:38:48 -0400

    point calculations

---
+++ index.js
@@ -0,0 +1,36 @@
+let homePoints = document.getElementById("home-points");
+let guestPoints = document.getElementById("guest-points");
+let totalHome = 0;
+let totalGuest = 0;
+
+// Calculations for the Home team's points
+function home1Point(){
+    totalHome += 1;
+    homePoints.innerText = totalHome;
+}
+
+function home2Points(){
+    totalHome += 2;
+    homePoints.innerText = totalHome;
+}
+
+function home3Points(){
+    totalHome += 3;
+    homePoints.innerText = totalHome;
+}
+
+// Calculations for the Guest team's points
+function guest1Point(){
+    totalGuest += 1;
+    guestPoints.innerText = totalGuest;
+}
+
+function guest2Points(){
+    totalGuest += 2;
+    guestPoints.innerText = totalGuest;
+}
+
+function guest3Points(){
+    totalGuest += 3;
+    guestPoints.innerText = totalGuest;
+}
